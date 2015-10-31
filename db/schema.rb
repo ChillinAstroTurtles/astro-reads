4289d70dffbd23021a766574538fb987eb3da950
Bruce <brucepark0@gmail.com>
2015-10-30 20:24:39 -0700

    Create Author model
    - Write tests that check for author object properties

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028070305) do
+ActiveRecord::Schema.define(version: 20151031031905) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "birth_country"
+    t.string   "gender"
+    t.string   "website"
+    t.string   "twitter_username"
+    t.string   "author_biography"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "delayed_jobs", force: :cascade do |t|
-    t.integer "priority",   default: 0, null: false
-    t.integer "attempts",   default: 0, null: false
-    t.text "handler",                null: false
-    t.text "last_error"
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
     t.datetime "run_at"
     t.datetime "locked_at"
     t.datetime "failed_at"
-    t.string "locked_by"
-    t.string "queue"
+    t.string   "locked_by"
+    t.string   "queue"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
 end
