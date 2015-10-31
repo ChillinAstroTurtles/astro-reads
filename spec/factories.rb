420497f008b750b0d4ad974c4d494661703fdbe5
Bruce <brucepark0@gmail.com>
2015-10-30 19:22:16 -0700

    Create author model
    - Add tests for author object properties
    - Alter database.yml with username and password for local migration

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,13 @@
 FactoryGirl.define do
+
+  factory :author do
+    birth_country "United States"
+    gender "Female"
+    website "http://www.google.com/"
+    twitter_username "@bpark0"
+    author_biography "Someone was born"
+    sequence(:first_name) { |n| "Firstname#{n}" }
+    sequence(:last_name)  { |n| "Lastname#{n}" }
+  end
+
 end
