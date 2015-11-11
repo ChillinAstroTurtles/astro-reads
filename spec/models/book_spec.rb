cf1040d9ca8b3b77873b672f4cc8c40d1d5f2fae
Daniel Strunk <me@dstrunk.com>
2015-11-10 19:17:16 -0500

    add model specs for Book and Category

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe Book, type: :model do
+  it { should respond_to(:name) }
+  it { should respond_to(:description) }
+  it { should have_many(:categories).through(:categorizations) }
+end
